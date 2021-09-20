67099b8d7e3cffb976926d27c804c8083ff5c782
sgash708 <sugash7081023@gmail.com>
2021-09-20 12:24:36 +0900

    プロジェクト作成

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import NotFound from '@/views/NotFound.vue'
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: NotFound
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Top msg="Welcome to the SpaAllegro"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Top from '@/components/Top.vue';
+
+export default Vue.extend({
+  name: 'Home',
+  components: {
+    Top,
+  },
+});
+</script>
