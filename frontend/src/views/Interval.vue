e837fcadccb9d935dac433e62a4e88f09c11f190
sgash708 <sugash7081023@gmail.com>
2021-09-23 21:37:00 +0900

    各ページ定義

---
+++ frontend/src/views/Interval.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="interval">
+    <h1>This is an "interval" page</h1>
+    <p>One of features is coming soon...</p>
+  </div>
+</template>
---
+++ frontend/src/views/Note.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="note">
+    <h1>This is an "note" page</h1>
+    <p>One of features is coming soon...</p>
+  </div>
+</template>
