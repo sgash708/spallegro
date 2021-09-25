b31c489b1064134df4931cba1fee8760fb6e6564
sgash708 <sugash7081023@gmail.com>
2021-09-25 11:36:14 +0900

    interval追加

--- frontend/src/components/TwoNotes.vue
+++ frontend/src/components/TwoNotes.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="twonotes">
+    <div class="explanation">
+      <p>2音間のインターバルに関する問題を出題します。出題範囲を下記から選択できます。</p>
+      <p>2音間のインターバルに関する問題を出題します。出題範囲を下記から選択できます。</p>
+    </div>
+
+    <h1>音部記号</h1>
     <div v-for="(clef, i) in clefs" :key="i">
       <input :id="i" type="checkbox" :value="clef.name" v-model="selectedNotes">
       <label :for="clef.name">{{ clef.viewName }}</label>
+    </div>
+
+    <h1>音程</h1>
+    <div v-for="(quality, i) in qualities" :key="i">
+      <input :id="i" type="checkbox" :value="quality.name" v-model="selectedQualities">
+      <label :for="quality.name">{{ quality.viewName }}</label>
+    </div>
+
+    <h1>インターバル</h1>
+    <div v-for="(quality, i) in qualities" :key="i">
+      <input :id="i" type="checkbox" :value="quality.name" v-model="selectedQualities">
+      <label :for="quality.name">{{ quality.viewName }}</label>
     </div>
 
     <transition name="modal" appear>
@@ -46,7 +64,73 @@ export default Vue.extend({
           viewName: 'テノール記号(Tnor)'
         },
       ],
-      selectedNotes: []
+      qualities: [
+        {
+          name: 'DoublyDiminished',
+          viewName: 'ダブルディミニッシュ(重減音程)'
+        },
+        {
+          name: 'Diminished',
+          viewName: 'ディミニッシュ(減音程)'
+        },
+        {
+          name: 'Perfect',
+          viewName: 'パーフェクト(完全音程)'
+        },
+        {
+          name: 'Major',
+          viewName: 'メジャー(長音程)'
+        },
+        {
+          name: 'Minor',
+          viewName: 'マイナー(短音程)'
+        },
+        {
+          name: 'Augmented',
+          viewName: 'オーギュメント(増音程)'
+        },
+        {
+          name: 'DoublyAugmented',
+          viewName: 'ダブルオーギュメント(重増音程)'
+        },
+      ],
+      intervals: [
+        {
+          name: '2nds',
+          viewName: '2度'
+        },
+        {
+          name: '3rds',
+          viewName: '3度'
+        },
+        {
+          name: '4ths',
+          viewName: '4度'
+        },
+        {
+          name: '5ths',
+          viewName: '5度'
+        },
+        {
+          name: '6ths',
+          viewName: '6度'
+        },
+        {
+          name: '7ths',
+          viewName: '7度'
+        },
+        {
+          name: '8ves',
+          viewName: '8度'
+        },
+        {
+          name: '9ths',
+          viewName: '9度'
+        },
+      ],
+      selectedNotes: [],
+      selectedQualities: [],
+      selectedIntervals: [],
     }
   },
 });
