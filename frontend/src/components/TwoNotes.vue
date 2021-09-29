8542fcc235ca86889d763636c768f683edc66d6a
sgash708 <sugash7081023@gmail.com>
2021-09-29 17:27:09 +0900

    カラム調整

--- frontend/src/components/TwoNotes.vue
+++ frontend/src/components/TwoNotes.vue
@@ -5,28 +5,28 @@
       <p>選択後に出題ボタンを押すと、開始します。</p>
     </div>
 
-    <h1 v-on:mouseenter="mouseOn()" v-on:mouseleave="mouseLeave()">音部記号</h1>
-    <p v-if="hoverFlag" id="balloon">音部記号とは、00000のことです。</p>
+    <h1 v-on:mouseenter="mouseOn(1)" v-on:mouseleave="mouseLeave()">音部記号</h1>
+    <p v-if="hoverFlag && 1 == hoverIndex" id="balloon">音部記号とは、00000のことです。</p>
     <div v-for="(clef, i) in clefs" :key="i">
       <input :id="i" type="checkbox" :value="clef.name" v-model="selectedNotes">
       <label :for="clef.name">{{ clef.viewName }}</label>
     </div>
 
-    <h1>音程</h1>
-    <p v-if="hoverFlag" id="balloon">音程とは、00000のことです。</p>
+    <h1 v-on:mouseenter="mouseOn(2)" v-on:mouseleave="mouseLeave()">音程</h1>
+    <p v-if="hoverFlag && 2 == hoverIndex" id="balloon">音程とは、00000のことです。</p>
     <div v-for="(quality, i) in qualities" :key="i">
       <input :id="i" type="checkbox" :value="quality.name" v-model="selectedQualities">
       <label :for="quality.name">{{ quality.viewName }}</label>
     </div>
 
-    <h1>インターバル</h1>
-    <p v-if="hoverFlag" id="balloon">インターバルとは、00000のことです。</p>
+    <h1 v-on:mouseenter="mouseOn(3)" v-on:mouseleave="mouseLeave()">インターバル</h1>
+    <p v-if="hoverFlag && 3 == hoverIndex" id="balloon">インターバルとは、00000のことです。</p>
     <div v-for="(interval, i) in intervals" :key="i">
       <input :id="i" type="checkbox" :value="interval.name" v-model="selectedIntervals">
       <label :for="interval.name">{{ interval.viewName }}</label>
     </div>
 
-    <transition name="modal" appear>
+    <!-- <transition name="modal" appear>
       <div class="modal modal-overlay" @click.self="$emit('close')">
         <div class="modal-window">
           <div class="modal-content">
@@ -38,114 +38,116 @@
           </footer>
         </div>
       </div>
-    </transition>
+    </transition> -->
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import { Component, Vue } from 'vue-property-decorator';
 
-export default Vue.extend({
-  name: 'Interval',
-  data() {
-    return {
-      hoverFlag: false,
-      clefs: [
-        {
-          name: 'treble',
-          viewName: 'トレブル記号(Treble)'
-        },
-        {
-          name: 'bass',
-          viewName: 'バス記号(Bass)'
-        },
-        {
-          name: 'Alto',
-          viewName: 'アルト記号(Alto)'
-        },
-        {
-          name: 'Tnor',
-          viewName: 'テノール記号(Tnor)'
-        },
-      ],
-      qualities: [
-        {
-          name: 'DoublyDiminished',
-          viewName: 'ダブルディミニッシュ(重減音程)'
-        },
-        {
-          name: 'Diminished',
-          viewName: 'ディミニッシュ(減音程)'
-        },
-        {
-          name: 'Perfect',
-          viewName: 'パーフェクト(完全音程)'
-        },
-        {
-          name: 'Major',
-          viewName: 'メジャー(長音程)'
-        },
-        {
-          name: 'Minor',
-          viewName: 'マイナー(短音程)'
-        },
-        {
-          name: 'Augmented',
-          viewName: 'オーギュメント(増音程)'
-        },
-        {
-          name: 'DoublyAugmented',
-          viewName: 'ダブルオーギュメント(重増音程)'
-        },
-      ],
-      intervals: [
-        {
-          name: '2nds',
-          viewName: '2度'
-        },
-        {
-          name: '3rds',
-          viewName: '3度'
-        },
-        {
-          name: '4ths',
-          viewName: '4度'
-        },
-        {
-          name: '5ths',
-          viewName: '5度'
-        },
-        {
-          name: '6ths',
-          viewName: '6度'
-        },
-        {
-          name: '7ths',
-          viewName: '7度'
-        },
-        {
-          name: '8ves',
-          viewName: '8度'
-        },
-        {
-          name: '9ths',
-          viewName: '9度'
-        },
-      ],
-      selectedNotes: [],
-      selectedQualities: [],
-      selectedIntervals: [],
-    }
-  },
-  methods: {
-    mouseOn() {
-      this.hoverFlag = true
+type Column = {
+  name: string;
+  viewName: string;
+}
+
+@Component
+export default class TwoNotes extends Vue{
+  // hoverFlag: boolean = false;
+  // REF: https://okkuso.com/vscode-error-no-inferrable-types/
+  hoverFlag = false
+  // REF: https://qiita.com/TK-C/items/edb7dc7d796b07811b78
+  hoverIndex = 0
+  clefs: Column[] = [
+    {
+      name: 'Treble',
+      viewName: 'トレブル記号(Treble)'
     },
-    mouseLeave() {
-      this.hoverFlag = false
-    }
+    {
+      name: 'Bass',
+      viewName: 'バス記号(Bass)'
+    },
+    {
+      name: 'Alto',
+      viewName: 'アルト記号(Alto)'
+    },
+    {
+      name: 'Tnor',
+      viewName: 'テノール記号(Tnor)'
+    },
+  ];
+  qualities: Column[] = [
+    {
+      name: 'DoublyDiminished',
+      viewName: 'ダブルディミニッシュ(重減音程)'
+    },
+    {
+      name: 'Diminished',
+      viewName: 'ディミニッシュ(減音程)'
+    },
+    {
+      name: 'Perfect/Major/Minor',
+      viewName: 'パーフェクト(完全音程)/メジャー(長音程)/マイナー(短音程)'
+    },
+    {
+      name: 'Augmented',
+      viewName: 'オーギュメント(増音程)'
+    },
+    {
+      name: 'DoublyAugmented',
+      viewName: 'ダブルオーギュメント(重増音程)'
+    },
+  ];
+  intervals: Column[] = [
+    {
+      name: '2nds',
+      viewName: '2度'
+    },
+    {
+      name: '3rds',
+      viewName: '3度'
+    },
+    {
+      name: '4ths',
+      viewName: '4度'
+    },
+    {
+      name: '5ths',
+      viewName: '5度'
+    },
+    {
+      name: '6ths',
+      viewName: '6度'
+    },
+    {
+      name: '7ths',
+      viewName: '7度'
+    },
+    {
+      name: '8ves',
+      viewName: '8度'
+    },
+    {
+      name: '9ths',
+      viewName: '9度'
+    },
+  ];
+  selectedNotes: string[] = [];
+  selectedQualities: string[] = [];
+  selectedIntervals: string[] = [];
+
+  /**
+   * Methods
+   * 返り値の型表現は、「: void」とすること
+   */
+  mouseOn(index: number): void {
+    this.hoverFlag = true
+    this.hoverIndex = index
   }
-});
+  mouseLeave(): void {
+    this.hoverFlag = false
+  }
+
+}
 </script>
 
 <style>
@@ -202,17 +204,5 @@ export default Vue.extend({
       transform: translateY(-20px);
     }
   }
-  .balloon {
-    position: relative;
-    display: inline-block;
-    margin: 1.5em 0;
-    padding: 7px 10px;
-    min-width: 120px;
-    max-width: 100%;
-    color: #555;
-    font-size: 16px;
-    background: #e0edff;
-    border-radius: 15px;
-}
 
 </style>
